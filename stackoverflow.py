a92c634a7e514326e2972d5682aa1c157a58080a
Tan Duc Mai <tan.duc.work@gmail.com>
2022-04-25 13:14:37 +0930

    Change heading description

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# ----------------------------------------------------------------------------
-# |
-# |        File:  stackoverflow.py
-# |      Author:  Tan Duc Mai
-# |       Email:  tan.duc.work@gmail.com
-# |        Date:  25-Apr-2022
-# | Description:  Paste the code snippet which contains an error into the
-# |               code_with_error() function.
-# |               The error will then be forwarded to stackoverflow website.
-# |   I hereby declare that I completed this work without any improper help
-# |   from a third party and without using any aids other than those cited.
-# |
-# ----------------------------------------------------------------------------
+# =============================================================================
+#
+#        FILE:  stackoverflow.py
+#      AUTHOR:  Tan Duc Mai
+#       EMAIL:  tan.duc.work@gmail.com
+#     CREATED:  25-Apr-2022
+# DESCRIPTION:  Search for the error found with stackoverflow.
+#       USAGE:  Paste the code snippet which contains an error into the
+#               code_with_error() function.
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+#
+# =============================================================================
 
 # ------------------------------- Module Import -------------------------------
 import webbrowser
