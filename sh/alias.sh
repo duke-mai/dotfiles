43b21271ffccbed2f278dfa39069f7493bd06ba9
Tan Duc Mai <tan.duc.work@gmail.com>
2022-04-27 23:00:25 +0930

    Add cmd to remove textual.log

--- sh/alias.sh
+++ sh/alias.sh
@@ -86,5 +86,5 @@ alias sortdict='printf "Finish sorting dictionary ...\n" && \
 alias pipinstall='python3 -m pip install --user --upgrade'
 alias pytree='python3 ~/.vim/py/tree.py'
 alias pydownload='python3 ~/.vim/py/downloader.py'
-alias pycalc='python3 ~/.vim/py/calculator.py'
+alias pycalc='python3 ~/.vim/py/calculator.py && rm textual.log'
 alias pyview='python3 ~/.vim/py/code_viewer.py && rm textual.log'
