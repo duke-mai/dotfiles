970532a6e30d48b29296c7cff2e7d7b721077a18
Tan Duc Mai <tan.duc.work@gmail.com>
2022-05-14 12:43:23 +0930

    Fix ls command

--- sh/alias.sh
+++ sh/alias.sh
@@ -28,12 +28,9 @@ fi
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # ls / exa aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 alias sl='sl && cl && ls'
 alias ls='exa'
-alias ll='exa -alh'
+alias ll='exa -alhF'
 alias tree='exa --tree'
 
 # other aliases
