6d10d365d071448c15788921a5268839eb6bbb5e
Tan Duc Mai <tan.duc.work@gmail.com>
2023-04-11 19:21:16 +0930

    Add CyberSecurity submodule

--- doc/CyberSecurity/Modern_Cryptography/Enigma/Python_Enigma/ReflectorA.py
+++
@@ -1,9 +0,0 @@
-#Reflector A
-
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-reflector = "EJMZALYXVBWFCRQUONTSPIKHGD"
-
-def reflect(letter):
-    spot = alphabet.find(letter)
-    return reflector[spot]
-
