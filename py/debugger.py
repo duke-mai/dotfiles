f613b476a9db220b16bffbb9fb5d6140074906e1
Henry Mai <henryfromvietnam@gmail.com>
2023-07-14 00:02:22 +0700

    style: add comments to code

--- py/debugger.py
+++ py/debugger.py
@@ -9,7 +9,7 @@
 #               code_with_error() function.
 #               To see the error in beautiful tracebacks, uncomment line 32 and
 #               comment out the last 4 lines.
-# DESCRIPTION:  Search for the error found with stackoverflow.
+# DESCRIPTION:  Lookup StackOverflow for the error found within code.
 #     CREATED:  2022-04-25
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
@@ -23,7 +23,8 @@ import webbrowser
 
 # ---------------------------- Function Definition ----------------------------
 def code_with_error():
-    pass
+    """Paste the code that results in an error here."""
+    print(1+'1')
 
 
 # --------------------------- Call the Main Function --------------------------
