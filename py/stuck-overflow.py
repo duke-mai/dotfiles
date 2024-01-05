d2ab169a039cf4ed5c67c542e425e32ae310afc6
Henry Mai <henryfromvietnam@gmail.com>
2024-01-05 20:18:20 +1030

    style(python): new name for file debugger.py

--- py/stuck-overflow.py
+++ py/stuck-overflow.py
@@ -3,12 +3,12 @@
 
 # =============================================================================
 #
-#        FILE:  debugger.py
+#        FILE:  stuck-overflow.py
 #      AUTHOR:  Henry Mai <henryfromvietnam@gmail.com>
 #       USAGE:  Paste the code snippet which contains an error into the
 #               code_with_error() function.
-#               To see the error in beautiful tracebacks, uncomment line 32 and
-#               comment out the last 4 lines.
+#               To see the error in beautiful tracebacks, uncomment line 37 and
+#               comment out the try-except statements.
 # DESCRIPTION:  Lookup StackOverflow for the error found within code.
 #     CREATED:  2022-04-25
 #   I hereby declare that I completed this work without any improper help
@@ -17,20 +17,30 @@
 # =============================================================================
 
 
-# ------------------------------- Module Import -------------------------------
+# ------------------------------- Module Imports ------------------------------
 import webbrowser
+
+# Enhance tracebacks
+from rich.traceback import install
+
+install(show_locals=True)
 
 
 # ---------------------------- Function Definition ----------------------------
 def code_with_error():
     """Paste the code that results in an error here."""
-    print(1+'1')
+    print(1 + "1")
 
 
-# -------------------------- Call the Main Function ---------------------------
-if __name__ == '__main__':
+# ------------------------------- Main Function -------------------------------
+def main():
     # code_with_error()
     try:
         code_with_error()
     except Exception as e:
-        webbrowser.open('http://stackoverflow.com/search?q=[python] ' + str(e))
+        webbrowser.open("http://stackoverflow.com/search?q=[python] " + str(e))
+
+
+# -------------------------- Call the Main Function ---------------------------
+if __name__ == '__main__':
+    main()
